6d67eb463048131d97a66c13b12a672b1f54bc4f
GabrielDiasGomes <131376484+GabrielDiasGomes@users.noreply.github.com>
2024-10-28 14:00:21 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import { NavBar } from '../components/Header/Navbar'
 import { LandingPageHero } from '../components/LandingPageHero/LandingPageHero'
 import { Footer } from '../components/Footer/Footer'
-
+import { CursosSe } from '../components/CursosSe/CursosSe';
 export function Home() {
   return (
     <>
       <NavBar />
       <LandingPageHero />
+      <CursosSe/>
       <Footer />
     </>
   )
