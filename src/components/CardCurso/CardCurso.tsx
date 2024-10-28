724728c71575963393f7daefcf2ae7fe463ebcf6
André <andrearistoteles0711@gmail.com>
2024-10-28 18:07:01 -0300

    :adhesive_bandage: fixed component key error

--- src/components/CardCurso/CardCurso.tsx
+++ src/components/CardCurso/CardCurso.tsx
@@ -5,12 +5,10 @@ type props = {
 
 function CardCurso(props: props) {
   return (
-    <div className="flex flex-col justify-center items-center m-3 ease-in-out duration-300 hover:scale-110">
-      <button className="w-64 h-64 transition-all">
+      <button className="w-64 h-64 flex flex-col justify-center items-center m-3 ease-in-out duration-300 hover:scale-110">
         <img src={props.img} alt={props.curName} className="rounded-3xl" />
         <p className="w-64 h-10 text-white m-3 text-center ">{props.curName}</p>
       </button>
-    </div>
   );
 }
 
