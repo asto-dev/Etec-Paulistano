8880050663912d4756eb4e674b589c6d7f1ce47c
André <andrearistoteles0711@gmail.com>
2024-10-28 18:09:14 -0300

    :lipstick: fixed navbar z-index problem

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -3,7 +3,7 @@ import Button from "../Button"
 
 export function NavBar() {
   return ( 
-    <header className='bg-blue-1000 opacity-90 w-dvw h-24 font-primary flex justify-between items-center px-14 font-semibold shadow-sm shadow-black fixed'>
+    <header className='bg-blue-1000 opacity-90 w-dvw h-24 font-primary flex justify-between items-center px-14 font-semibold shadow-sm shadow-black fixed z-10'>
       <img src={etecLogo} alt="Logo da Etec" />
 
       <nav>
