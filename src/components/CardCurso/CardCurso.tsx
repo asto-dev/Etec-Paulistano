99c39982450bb70f368b6a2c7f6e1a1354a69ef3
André <andrearistoteles0711@gmail.com>
2024-10-28 17:57:04 -0300

    💄 fixed hover effect on cursos section

--- src/components/CardCurso/CardCurso.tsx
+++ src/components/CardCurso/CardCurso.tsx
@@ -5,13 +5,11 @@ type props = {
 
 function CardCurso(props: props) {
   return (
-    <div className="flex flex-col justify-center items-center m-3">
-      <button className="w-64 h-64 hover:w-80 transition-all">
+    <div className="flex flex-col justify-center items-center m-3 ease-in-out duration-300 hover:scale-110">
+      <button className="w-64 h-64 transition-all">
         <img src={props.img} alt={props.curName} className="rounded-3xl" />
+        <p className="w-64 h-10 text-white m-3 text-center ">{props.curName}</p>
       </button>
-      <div className="w-64 h-10 text-white m-3 flex justify-center items-center ">
-        {props.curName}
-      </div>
     </div>
   );
 }
