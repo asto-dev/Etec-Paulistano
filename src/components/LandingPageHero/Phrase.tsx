5690d030ab8d88142bf6d7784398a09b293ec8ec
ALAN-MS-SU <fowodahs12314@outlook.com>
2024-10-28 21:42:10 -0300

    add Phrase by Paulo Freire

---
+++ src/components/LandingPageHero/Phrase.tsx
@@ -0,0 +1,23 @@
+import { BiSolidQuoteAltLeft, BiSolidQuoteAltRight } from "react-icons/bi";
+export function Phrase(props:{bg:string,text:string}) {
+  return (
+    <section
+      id="Phrase"
+      className=" flex flex-row justify-center p-[150px] gap-[350px]"
+      style={{backgroundColor:props.bg}}
+    >
+      <div className="flex flex-col items-center">
+        <BiSolidQuoteAltLeft className="  text-yellow-400 text-[100px]  relative right-[370px] top-[50px]" />
+        <h1 style={{color:props.text}} className=" text-5xl text-center font-sans text-nowrap">
+          Se a educação sozinha não <br /> transforma a sociedade, sem <br />
+          ela tampouco a sociedade <br /> muda.
+        </h1>
+        <BiSolidQuoteAltRight className=" text-yellow-400 text-[100px]  relative left-[370px] bottom-[50px] " />
+      </div>
+      <img
+        src="./paulo-freire.png"
+        className=" w-[300px] h-[300px] object-fill border-2 rounded-full border-[--text-yellow]"
+      />
+    </section>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import { NavBar } from '../components/Header/Navbar'
 import { LandingPageHero } from '../components/LandingPageHero/LandingPageHero'
 import { Footer } from '../components/Footer/Footer'
+import { Phrase } from '../components/LandingPageHero/Phrase'
 
 export function Home() {
   return (
     <>
       <NavBar />
       <LandingPageHero />
+      <Phrase bg={'var(--light-bg)'} text={'black'}/>
       <Footer />
     </>
   )
