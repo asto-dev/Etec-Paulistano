b0fb326e9cf95395fc9e24de3db09f6d26238065
ALAN-MS-SU <fowodahs12314@outlook.com>
2024-11-01 19:19:59 -0300

    Create Hero Courses

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,9 +1,12 @@
-import { NavBar } from '../components/shared/Navbar/Navbar'
-
+import Hero from "../components/pages/Couses/hero";
+import { NavBar } from "../components/shared/Navbar/Navbar";
 export function Courses() {
+  {
+  }
   return (
     <>
       <NavBar />
+      <Hero Name="Desenvolvimento de sistemas" />
     </>
-  )
+  );
 }
